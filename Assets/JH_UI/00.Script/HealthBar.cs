a77df6f4d9415c7379382f9e341c4e749378926c
Chamom1ie <GitHubmile@gmail.com>
2024-03-09 19:52:34 +0900

    add summary
    in HealthBar.cs

--- Assets/JH_UI/00.Script/HealthBar.cs
+++ Assets/JH_UI/00.Script/HealthBar.cs
@@ -14,27 +14,49 @@ public class HealthBar : MonoBehaviour
         _fill.color = _gradient.Evaluate(1f);
         Init(100);
     }
+    /// <summary>
+    /// Initialize HealthBar slider value to maxHealth
+    /// </summary>
+    /// <param name="maxHealth">최대 체력</param>
     public void Init(int maxHealth)
     {
         _slider.maxValue = maxHealth;
         _slider.value = maxHealth;
     }
+
+    /// <summary>
+    /// Set healthBar slider value to health
+    /// </summary>
+    /// <param name="health">체력</param>
     public void SetHealth(int health)
     {
         _slider.value = health;
         SetGradientValue();
     }
+
+    /// <summary>
+    /// Decrease healthBar slider value to amount
+    /// </summary>
+    /// <param name="amount">값</param>
     public void DecreaseHealth(int amount)
     {
         _slider.value -= amount;
         SetGradientValue();
     }
 
+    /// <summary>
+    /// Increase healthBar slider value to amount
+    /// </summary>
+    /// <param name="amount">값</param>
     public void IncreaseHealth(int amount)
     {
         _slider.value += amount;
         SetGradientValue();
     }
+
+    /// <summary>
+    /// Set slider's color to the slider's normalizedvalue
+    /// </summary>
     public void SetGradientValue()
     {
         _fill.color = _gradient.Evaluate(_slider.normalizedValue);
