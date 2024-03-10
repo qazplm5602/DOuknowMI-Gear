daea5a8a24a17bc7ee563f2e9c3f8bdee800d6ba
qazplm5602 <t01048187045@gmail.com>
2024-03-11 00:30:11 +0900

    기획과 다른 SO

--- Assets/Temp_Attack/Scripts/CombineLinkCogData.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "SO/Cog/CombineLink")]
-public class CombineLinkCogData : CogBaseData {
-    public GameObject loadMoudle;
-    public LinkCogData[] comnineCog;
-}
--- Assets/Temp_Attack/Scripts/LinkCogData.cs
+++
@@ -1,4 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "SO/Cog/Link")]
-public class LinkCogData : CogBaseData {}
