ceadf9668d2afcd5c8ded159e16c9d1db6abeb37
Chamom1ie <GitHubmile@gmail.com>
2024-03-21 20:05:51 +0900

    Make tag Wall
    기어 4개 체인 3개는 만들었음

--- Assets/Temp_Attack/SO/Multiplies3/LinkOneTwoThreeFour/TestLinkT3.cs
+++ Assets/Temp_Attack/SO/Multiplies3/LinkOneTwoThreeFour/TestLinkT3.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestLinkT2 : GearCogEvent
+public class TestLinkT3 : GearCogEvent
 {
     public override void Use()
     {
-        print("[Link] TestLinkT2 호출");
+        print("[Link] TestLinkT3 호출");
     }
 }
