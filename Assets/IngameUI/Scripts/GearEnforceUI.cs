ffe2561101caa9e394d4a327469ec00835348f59
qazplm5602 <t01048187045@gmail.com>
2024-04-12 01:09:38 +0900

    기어 강화 화면 표시

--- Assets/IngameUI/Scripts/GearEnforceUI.cs
+++ Assets/IngameUI/Scripts/GearEnforceUI.cs
@@ -1,8 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine;
+using System;
 
 public class GearEnforceUI : MonoBehaviour
 {
@@ -15,21 +14,32 @@ public class GearEnforceUI : MonoBehaviour
     
     [SerializeField] int[] _paymentCoin;
 
+    // 이건 기어 교체창하고 연동하기 전까지는 임시로 사용하는 변수 (그니까 바로 키면 이 기어로 켜짐)
+    [SerializeField, System.Obsolete] GearSO tempGear;
+
     /////////////// cache
     TextMeshProUGUI subtitle;
     Image paySkillIco;
     TextMeshProUGUI payNasaT;
+
+    GearGroupDTO currentGear;
+    Action<bool> callback;
     
     private void Awake() {
         subtitle = _mainBox.transform.Find("question").GetComponent<TextMeshProUGUI>();
 
         paySkillIco = _payBoxSkill.transform.Find("Img").GetComponent<Image>();
-        payNasaT = _payBoxSkill.transform.Find("Text").GetComponent<TextMeshProUGUI>();
+        payNasaT = _payBoxNasa.transform.Find("Text").GetComponent<TextMeshProUGUI>();
+    
+        // 테스트 코드
+        Show(new GearGroupDTO() { data = tempGear, stat = new() { level = 1 } });
     }
 
     public void Show(GearGroupDTO gear) {
+        currentGear = gear;
         subtitle.text = $"{gear.data.Name}의 기어를 강화 하시겠습니까?";
 
+        // 기어 박스 설정
         _nowSkill.SetSkill(gear);
 
         GearStat currentStat = gear.stat;
@@ -38,12 +48,21 @@ public class GearEnforceUI : MonoBehaviour
             data = gear.data,
             stat = currentStat
         });
+        
+        // 소모 아이템 설정
+        _payBoxSkill.SetActive(gear.stat.level == 4);
+        if (gear.stat.level == 4) {
+            paySkillIco.sprite = gear.data.Icon;
+        }
 
+        payNasaT.text = $"<color=red>0</color> / {_paymentCoin[gear.stat.level]}";
 
         _mainBox.SetActive(true);
     }
 
     public void Hide() {
+        currentGear = null;
+        callback = null;
         _mainBox.SetActive(false);
     }
 }
