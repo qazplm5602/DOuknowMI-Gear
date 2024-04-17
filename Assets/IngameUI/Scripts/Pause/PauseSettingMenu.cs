ef7f7425f5d2d0c0005ee50ef69ad88f9853e305
qazplm5602 <t01048187045@gmail.com>
2024-04-18 01:51:07 +0900

    이제 설정 박스 만들긱

--- Assets/IngameUI/Scripts/Pause/PauseSettingMenu.cs
+++ Assets/IngameUI/Scripts/Pause/PauseSettingMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
 struct PauseSettingSOPair {
@@ -17,8 +18,40 @@ public class PauseSettingMenu : MonoBehaviour
     [SerializeField] TextMeshProUGUI _description;
 
     [Header("Prefab")]
+    [SerializeField] Button _sectionBtn;
     [SerializeField] GameObject _box;
     
     [Header("Config")]
     [SerializeField] PauseSettingSOPair[] menus;
+
+    private void Awake() {
+        for (int i = 0; i < menus.Length; i++)
+        {
+            int idx = i;
+            var item = menus[i];
+
+            Button menu = Instantiate(_sectionBtn, _categorys);
+            menu.onClick.AddListener(() => ClickCategory(idx));
+            
+            var text = menu.GetComponent<TextMeshProUGUI>();
+            text.color = new Color32(128, 128, 128, 255);
+            text.text = item.category;
+        }
+    }
+
+    void ClickCategory(int idx) {
+        int i = 0;
+        foreach (Transform item in _categorys)
+        {
+            item.GetComponent<TextMeshProUGUI>().color = (i == idx) ? Color.white : new Color32(128, 128, 128, 255);
+            i ++;
+        }
+
+        // 콘텐츠 안에 있는거 다 지움
+        foreach (Transform item in _scrollContent)
+            Destroy(item.gameObject);
+
+
+        
+    }
 }
