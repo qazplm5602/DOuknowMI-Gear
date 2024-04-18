91a2b2c032d4f585895136e318f127c52e674c23
Chamom1ie <GitHubmile@gmail.com>
2024-04-18 20:53:13 +0900

    use float and method

--- Assets/Z_JinHyunSkillKillKillKill/Scripts/GearSkills/Controllers/{Name}/S_Clock.cs
+++ Assets/Z_JinHyunSkillKillKillKill/Scripts/GearSkills/Controllers/{Name}/S_Clock.cs
@@ -13,7 +13,10 @@ public class SkillClock : SkillController
 
     protected override IEnumerator MoveRoutine(Transform startTrm)
     {
-        
+        ModifyEnemyStat(StatType.AttackSpeed, _slowPercent, true);
+        ModifyEnemyStat(StatType.MoveSpeed, _slowPercent, true);
         yield return StartCoroutine(base.MoveRoutine(startTrm));
+        ModifyEnemyStat(StatType.AttackSpeed, _slowPercent, false);
+        ModifyEnemyStat(StatType.MoveSpeed, _slowPercent, false);
     }
 }
