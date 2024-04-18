60ddece56f4301d52483befd8d6f9705f2fc7902
mki821 <alexalex0821@naver.com>
2024-04-18 19:58:15 +0900

    AudioMixer 추가

--- Assets/Core/SoundManager.cs
+++ Assets/Core/SoundManager.cs
@@ -1,9 +1,13 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SoundManager : MonoSingleton<SoundManager>
 {
     [SerializeField] private AudioClip[] _audios;
+    
+    [SerializeField] private AudioMixer _audioMixer;
+    [SerializeField] private AudioMixerGroup _bgmMixerGroup;
 
     private Dictionary<string, AudioClip> _audioClipDictionary;
     private AudioSource _audioSource;
@@ -16,6 +20,10 @@ public class SoundManager : MonoSingleton<SoundManager>
         foreach(AudioClip clip in _audios) {
             _audioClipDictionary.Add(clip.name, clip);
         }
+    }
+
+    public void SetVolume(string name, int value) {
+        _audioMixer.SetFloat(name, value);
     }
 
     public void PlaySound(string clipName) {
@@ -38,6 +46,7 @@ public class SoundManager : MonoSingleton<SoundManager>
         GameObject obj = new GameObject(clipName);
         obj.transform.SetParent(transform);
         _bgmPlayer = obj.AddComponent<AudioSource>();
+        _bgmPlayer.outputAudioMixerGroup = _bgmMixerGroup;
         _bgmPlayer.loop = true;
         _bgmPlayer.clip = _audioClipDictionary[clipName];
         _bgmPlayer.Play();
