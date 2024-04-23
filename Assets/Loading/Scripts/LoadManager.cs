6113600acc24f29a1ba3d2d3c5dd8ed8ce4bcc01
qazplm5602 <t01048187045@gmail.com>
2024-04-24 00:54:39 +0900

    로딩 화면 구성

---
+++ Assets/Loading/Scripts/LoadManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadManager : MonoBehaviour
+{
+    static string goScene;
+    
+    public static void LoadScene() {
+        
+    }
+}
