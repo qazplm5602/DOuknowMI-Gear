193af7788bf1803e204c196e4da9959c07b2c4f4
Chamom1ie <GitHubmile@gmail.com>
2024-04-29 00:53:07 +0900

    makinglensskill
    maybe거의다만듬

--- Assets/Z_JinHyunSkillKillKillKill/Scripts/GearSkills/Controllers/{Name}/S_Lens.cs
+++ Assets/Z_JinHyunSkillKillKillKill/Scripts/GearSkills/Controllers/{Name}/S_Lens.cs
@@ -1,11 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class SkillLens : SkillController
 {
+    [SerializeField] private LayerMask _weaponLayerMask;
     private void Start()
     {
         StartCoroutine(MoveRoutine(transform));
+    }
+
+    protected override void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == _weaponLayerMask)
+        {
+            Vector3 center = PlayerManager.instance.playerTrm.position;
+            Vector3 mapSizeHalfExtents = new Vector3(30, 15, 1);
+            //Collider[] enemies = Map.Instance.CurrentStage.CurrentEnemies; // 맵매니저에 현재맵의 뭐 가져오는거로할건데 일단 지금은 개똥쓰레기상태
+            
+            //foreach (var enemy in enemies)
+            //{
+            //    if (enemy.TryGetComponent(out IDamageable target))
+            //    {
+            //        target.ApplyDamage(Mathf.FloorToInt(_damage), null);
+            //    }
+            //}
+        }
     }
 }
