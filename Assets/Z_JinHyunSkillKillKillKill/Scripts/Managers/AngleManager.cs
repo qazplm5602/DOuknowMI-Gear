07fb3aa3ae8ef1e8856e9acf5023597756e5adb9
Chamom1ie <GitHubmile@gmail.com>
2024-04-30 01:00:56 +0900

    perspective view fix
    mmm now making camera

--- Assets/Z_JinHyunSkillKillKillKill/Scripts/Managers/AngleManager.cs
+++ Assets/Z_JinHyunSkillKillKillKill/Scripts/Managers/AngleManager.cs
@@ -5,8 +5,9 @@ public class AngleManager : MonoBehaviour
 {
     public static Quaternion GetTargetDirection(Vector3 playerPos, Vector3 mousePos)
     {
+
         Vector3 pPos = playerPos;
-        Vector3 mPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 mPos = Camera.main.transform.GetChild(0).GetComponent<Camera>().ScreenToWorldPoint(Input.mousePosition);
         float angle = Mathf.Atan2(mPos.y - pPos.y, mPos.x - pPos.x) * Mathf.Rad2Deg;
         Quaternion lookRotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
