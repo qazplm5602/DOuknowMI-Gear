b2e325db8a7c72dffe1802cf67c66675ba4cea45
mki821 <alexalex0821@naver.com>
2024-05-09 18:36:48 +0900

    EnemyLJ_K Column Sprite 수정

--- Assets/Enemy/0B_Scripts/FSM/Enemy/LJ-K/EnemyLJ_KColumn.cs
+++ Assets/Enemy/0B_Scripts/FSM/Enemy/LJ-K/EnemyLJ_KColumn.cs
@@ -3,21 +3,29 @@ using UnityEngine;
 
 public class EnemyLJ_KColumn : MonoBehaviour
 {
+    public int index = 0;
+
     [SerializeField] private float _lifeTime = 1f;
     [SerializeField] private float _appearTime;
     [SerializeField] private float _disappearTime;
     [SerializeField] private int _damage;
 
+    [SerializeField] private BoxCollider2D _collider;
+
     private bool _damageable = true;
 
     private void Start() {
+        transform.GetChild(0).GetChild(index).gameObject.SetActive(true);
+        _collider.offset = new Vector3(0, 0.5f + 0.5f * index);
+        _collider.size = new Vector3(3, 5 + index);
+
         StartCoroutine(Appear());
     }
 
     private IEnumerator Appear() {
         float timer = 0f;
         Vector2 originPosition = transform.position;
-        Vector2 endPosition = transform.position + Vector3.up * transform.localScale.y;
+        Vector2 endPosition = transform.position + Vector3.up * (7f + index);
 
         while(timer < _appearTime) {
             timer += Time.deltaTime;
--- Assets/PlayerStat/0B_Scripts/PlayerExperience.cs
+++ Assets/PlayerStat/0B_Scripts/PlayerExperience.cs
@@ -5,7 +5,7 @@ public class PlayerExperience : MonoBehaviour
 {
     public int level = 1;
     public int currentExp = 0;
-    [SerializeField] private int _needExp = 1;
+    [SerializeField] private int[] _needExp;
 
     [SerializeField] private PlayerStat _playerStat;
 
@@ -18,8 +18,8 @@ public class PlayerExperience : MonoBehaviour
     }
 
     private void CheckExp() {
-        if(currentExp >= _needExp) {
-            currentExp -= _needExp;
+        if(currentExp >= _needExp[level - 1]) {
+            currentExp -= _needExp[level - 1];
             ++level;
             _playerStat.statPoint += 3;
             
