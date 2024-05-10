0958274eb1eff011e2068f4379f837dcb159577b
Chamom1ie <GitHubmile@gmail.com>
2024-05-10 23:46:00 +0900

    Remove Using

--- Assets/JinHyunIn/JinHyunSkill/Scripts/GearSkills/Controllers/{Name}/S_Camera.cs
+++ Assets/JinHyunIn/JinHyunSkill/Scripts/GearSkills/Controllers/{Name}/S_Camera.cs
@@ -1,9 +1,7 @@
-using FSM;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.VisualScripting.ReorderableList;
 using UnityEngine;
 
 public class SkillCamera : SkillController
