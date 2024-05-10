4e86f70410a2cffddf2d72f4b6f430cd4666a141
Chamom1ie <GitHubmile@gmail.com>
2024-05-10 23:23:15 +0900

    ASMDKASMD

--- Assets/!Lego/01.Script/Village/VillageManager.cs
+++ Assets/!Lego/01.Script/Village/VillageManager.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,13 @@ using UnityEngine.SceneManagement;
 public class VillageManager : MonoSingleton<VillageManager>
 {
     [SerializeField] private string _stageSceneName;
+    [SerializeField] private PolygonCollider2D _VillageCollider;
+    [SerializeField] CinemachineConfiner2D confiner2d;
 
+    private void Start()
+    {
+        confiner2d.m_BoundingShape2D = _VillageCollider;
+    }
     public void ChangeSceneToStage()
     {
         SceneManager.LoadScene(_stageSceneName);
